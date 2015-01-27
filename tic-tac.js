a4328dc1ef00c8756eb13219992ceaffaaf531b8
Tyler Olson <tydotg@gmail.com>
2015-01-26 20:29:21 -0800

    Add render functionality

--- tic-tac.js
+++ tic-tac.js
@@ -1,3 +1,12 @@
-var Game = require('./lib/models/game');
+var Game = require('./lib/models/game'),
+    game = new Game();
 
-new Game();
+game
+  .use('random')
+  .onRender(renderWithConsole)
+  .start();
+
+function renderWithConsole() {
+  var values = this.board.map('value').join('\n');
+  console.log(values + '\n');
+}
